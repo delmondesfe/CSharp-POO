756a989e07630905a15da2ba2a3401f0af19a25d
DESKTOP-0M0NPU4\Felipe Delmondes <luis.delmondes@outlook.com>
2020-10-21 19:36:14 -0300

    Adicionando arquivo 4

---
+++ 04 - ByteBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04___ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+
+            contaDoBruno.Sacar(200);
+
+            Console.WriteLine(contaDoBruno.saldo);
+            Console.ReadLine();
+        }
+    }
+}
